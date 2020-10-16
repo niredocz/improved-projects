20633394858643ffcb4810de80ec56b515859e04
niredocz <daffa.rogers@gmail.com>
2020-10-17 05:56:47 +0700

    initial commit

---
+++ js-projects/js-trial folder/main.js
@@ -0,0 +1,9 @@
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = dob;
+}
+const person1 = new Person('Daffa', 'Fauzi', '02-11-1998');
+const person2 = new Person('Nabil', 'Alrafie', '16-10-2003');
+
+console.log(person2);
