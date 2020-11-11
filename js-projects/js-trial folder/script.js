1c09e070d211b38f0416e5fedd9d8386620a4057
Niredocz <daffa.rogers@gmail.com>
2020-11-11 11:58:38 +0700

    update filterOption

--- js-projects/js-trial folder/script.js
+++ js-projects/js-trial folder/script.js
@@ -63,26 +63,26 @@ function deleteTodo(e) {
 function filterTodo(e) {
     const todos = todoList.childNodes;
     todos.forEach(function(todo) {
-        switch (e.target.value) {
-            case "all":
-                todo.style.display = "flex";
-                break;
-            case "completed":
-                if (todo.classList.contains("completed")) {
-                    todo.style.display = "flex";
-                } else {
-                    todo.display.style = "none";
-                }
-                break;
-            case "uncompleted":
-                if (!todo.classList.contains("completed")) {
-                    todo.style.display = "flex";
-                } else {
-                    todo.display.style = "none";
-                }
-        }
+      switch (e.target.value) {
+        case "all":
+          todo.style.display = "flex";
+          break;
+        case "completed":
+          if (todo.classList.contains("completed")) {
+            todo.style.display = "flex";
+          } else {
+            todo.style.display = "none";
+          }
+          break;
+        case "uncompleted":
+          if (!todo.classList.contains("completed")) {
+            todo.style.display = "flex";
+          } else {
+            todo.style.display = "none";
+          }
+      }
     });
-}
+  }
 
 function saveLocalTodos(todo) {
     let todos;
